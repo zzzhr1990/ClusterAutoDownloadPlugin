aede81d0b2df81221ba5275fc7db8f2997e3929e
zzzhr <zzzhr@hotmail.com>
2017-03-12 15:19:00 +0800

    cntr

--- clusterautodownloadplugin/core.py
+++ clusterautodownloadplugin/core.py
@@ -139,8 +139,8 @@ class Core(CorePluginBase):
         filemanager = BucketManager(auth)
         filemanager.mgr_host = WorkConfig.MGR_HOST
         code,text = filemanager.stat(bucket, file_key)
-        print("file get %d, %s", code, text)
-        
+        print("file get from %s %d, %s",filemanager.mgr_host , code, text)
+
         # check file
         # confirm if this file uploaded
 
