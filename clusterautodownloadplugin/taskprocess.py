27a0ac1ee48ec8f9644a7b5eceacbc8cc9956932
zzzhr <zzzhr@hotmail.com>
2017-03-16 17:59:31 +0800

    F@CK

--- clusterautodownloadplugin/taskprocess.py
+++ clusterautodownloadplugin/taskprocess.py
@@ -10,19 +10,13 @@ class TaskProcess(object):
     def __init__(self, base_url):
         self._base_url = base_url
 
-    def check_tasks(self):
-        """Check tasks on server."""
-        req = requests.get(self._base_url + '/v1/task'\
-        , headers={"X-Task-Token" : "1024tasktoken"}, timeout=5)
+    def exec_requests_data_json(self, req):
+        """Execute standard request"""
         if req.status_code == 200:
             json_request = req.json()
             if json_request["errno"] == 0:
                 if "data" in json_request:
-                    data = json_request["data"]
-                    if "data" in data:
-                        data_arr = data["data"]
-                        for single_data in data_arr:
-                            self.fetch_single_task(single_data)
+                    return json_request["data"]
                     else:
                         log.warn("No data object found in response JSON['data'].\r\n%s", data)
                 else:
@@ -32,51 +26,83 @@ class TaskProcess(object):
                 json_request["errno"], json_request["errmsg"])
         else:
             log.warn("Data recv error, code:%d", req.status_code)
+        return None
+
+    def check_tasks(self):
+        """Check tasks on server."""
+        req = requests.get(self._base_url + '/v1/task'\
+        , headers={"X-Task-Token" : "1024tasktoken"}, timeout=5)
+        data = self.exec_requests_data_json(req)
+        if data != None:
+            if "data" in data:
+                data_arr = data["data"]
+                for single_data in data_arr:
+                    self.fetch_single_task(single_data)
+            else:
+                log.warn("No data object found in response JSON['data'].\r\n%s", data)
+        else:
+            log.warn("No data recv from check_tasks.")
 
     def fetch_single_task(self, single_task):
         """Check single task."""
         task_type = single_task["type"]
+        core = component.get("Core")
+        task_info = core.get_torrent_status(single_task["infohash"], {})
+        if task_info != None:
+            if len(task_info) == 0:
+                log.warn("Task info has no info, maybe it has been removed? %s"\
+                , single_task["tid"])
+                # Report to task server?
+                self.change_torrent_status(single_task["tid"]\
+                    , {"status" : 5, "infohash" : single_task["infohash"]})
+                return
+        else:
+            log.info("Torrent %s[%s] already in download list."\
+                , single_task["tid"], single_task["infohash"])
+            self.change_torrent_status(single_task["tid"]\
+                , {"status" : 5, "infohash" : single_task["infohash"]})
+                return
         if task_type == "torrent":
             req = requests.get(single_task["url"], \
             headers={"X-Task-Token" : "1024tasktoken"}, timeout=5)
             if req.status_code == 200:
                 try:
-                    core = component.get("Core")
-                    #find if the task exists
-                    task_info = core.get_torrent_status(single_task["infohash"], {})
-                    if task_info != None:
-                        if len(task_info) == 0:
-                            log.warn("Task info has no info, maybe it has been removed? %s"\
-                            , single_task["tid"])
-                            # Report to task server?
-                        else:
-                            log.info("Torrent %s[%s] already in download list."\
-                            , single_task["tid"], single_task["infohash"])
-                            self.change_torrent_status(single_task["tid"]\
-                            , {"status" : 5, "infohash" : single_task["infohash"]})
-                    else:
-                        log.info("Adding torrent %s[%s] to download list."\
+                    log.info("Adding torrent %s[%s] to download list."\
                         , single_task["tid"], single_task["infohash"])
-                        torrent_id = core.add_torrent_file(single_task["tid"],\
-                        base64.encodestring(req.content), {})
-                        if torrent_id != None:
-                            log.info("Successfly add torrent, tid: %s", single_task["tid"])
-                            self.change_torrent_status(single_task["tid"]\
+                    torrent_id = core.add_torrent_file(single_task["tid"],\
+                    base64.encodestring(req.content), {})
+                    if torrent_id != None:
+                        log.info("Successfly add torrent, tid: %s", single_task["tid"])
+                        self.change_torrent_status(single_task["tid"]\
                             , {"status" : 5, "infohash" : torrent_id})
-                        else:
-                            log.info(\
+                    else:
+                        log.info(\
                             "Torrent %s[%s] already in download list but not figured before."\
                             , single_task["tid"], single_task["infohash"])
                 except Exception as ex:
                     log.error("Unable to add torrent file!: %s", ex)
                     return
+            else:
+                log.info("Add torrent file error.")
+        if task_type == "magnet":
+            try:
+                torrent_id = core.add_torrent_magnet(single_task["url"], {})
+                if torrent_id != None:
+                    log.info("Successfly add magnet, tid: %s", single_task["tid"])
+                    self.change_torrent_status(single_task["tid"]\
+                        , {"status" : 5, "infohash" : torrent_id})
+                else:
+                    log.warn(\
+                        "Torrent %s[%s] already in download list but not figured before."\
+                        , single_task["tid"], single_task["infohash"])
+            except Exception as ex:
+                log.error("Unable to add torrent file!: %s", ex)
+        else:
+            pass
 
     def change_torrent_status(self, tid, torrent_info):
         """Change the torrent status on server."""
         req = requests.put(self._base_url + '/v1/task/' + tid\
         , headers={"X-Task-Token" : "1024tasktoken"}, timeout=5, json=torrent_info)
-        if req.status_code == 200:
-            log.info("Received from server %s", req.json())
-        else:
-            log.info("Received HTTP error %d.", req.status_code)
+        self.exec_requests_data_json(req)
 
