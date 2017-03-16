5ee2d1d6a2596a57c41a8c9b1d11a93ed228238d
zzzhr <zzzhr@hotmail.com>
2017-03-17 01:21:37 +0800

    ccp

--- clusterautodownloadplugin/torrentprocesser.py
+++ clusterautodownloadplugin/torrentprocesser.py
@@ -24,7 +24,7 @@ class TorrentProcesser(Process):
         self.disable = False
         self.current_upload = None
         self.torrent_info = torrent_info
-        super(TorrentProcesser).__init__()
+        super(TorrentProcesser).__init__(self)
 
     def stop(self):
         """Stop"""
