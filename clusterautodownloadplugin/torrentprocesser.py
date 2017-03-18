485f42ae711009cfa9dd43461ec640ae97a4adde
zzzhr <zzzhr@hotmail.com>
2017-03-19 02:24:03 +0800

    ss

--- clusterautodownloadplugin/torrentprocesser.py
+++ clusterautodownloadplugin/torrentprocesser.py
@@ -31,7 +31,6 @@ class TorrentProcesser(Process):
 
     def _loop(self):
         while True:
-            log.info("CHECKING____%d", self.command_queue.empty())
             if not self.command_queue.empty():
                 log.info("Torrent process %d terminated.", self.process_id)
                 self.terminated = True
