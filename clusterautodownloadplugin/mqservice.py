14823c6adfff76883414dacdd81ec1c21e2f43a3
古娜 <shiti08@126.com>
2017-05-15 00:42:03 +0800

    fix

--- clusterautodownloadplugin/mqservice.py
+++ clusterautodownloadplugin/mqservice.py
@@ -95,7 +95,7 @@ class MqService(ConsumerProducerMixin):
             logging.info("Added torrent success %s", torrent_id)
             # Get Torrent File Info
             file_data = self.deluge_api.get_torrent_status(
-                torrent_id, [])
+                torrent_id, ["name", "hash", "files", "file_priorities"])
             self._delive_torrent_parse_success(info["hash"], file_data, info)
         except RuntimeError as ex:
             logging.warning(
