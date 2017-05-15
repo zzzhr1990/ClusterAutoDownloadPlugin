489dc41a6a2e28c4c1543b46b5d551079a33e72d
古娜 <shiti08@126.com>
2017-05-15 22:12:19 +0800

    dixs

--- clusterautodownloadplugin/mqservice.py
+++ clusterautodownloadplugin/mqservice.py
@@ -27,7 +27,7 @@ class MqService(ConsumerProducerMixin):
             hostname=mq_host, port=mq_port, password=mq_password, userid=mq_user)
         self.torrent_queue(self.connection).declare()
         self.server_id = server_id
-        deluge_api.register_event_handler.register_event_handler(
+        deluge_api.eventmanager.register_event_handler(
             "TorrentFileRenamedEvent", self._on_torrent_rename)
         # listen to TorrentFileRenamedEvent
 
