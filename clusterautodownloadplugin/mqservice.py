5993c12f98f2c515ceeb807996f6c77e3c498d77
古娜 <shiti08@126.com>
2017-05-15 14:51:23 +0800

    test torrent

--- clusterautodownloadplugin/mqservice.py
+++ clusterautodownloadplugin/mqservice.py
@@ -90,13 +90,14 @@ class MqService(ConsumerProducerMixin):
                 logging.info("%s existed.", torrent_hash)
                 torrent_id = torrent_hash
             else:
-                logging.warn(
-                    "Torrent id mismatch!!!! rechange....%s", json.dumps(info))
+                if torrent_hash != torrent_id:
+                    logging.warn(
+                        "Torrent id mismatch!!!! rechange....%s", json.dumps(info))
             logging.info("Added torrent success %s", torrent_id)
             # Get Torrent File Info
             file_data = self.deluge_api.get_torrent_status(
                 torrent_id,
-                ["name", "hash", "files", "file_priorities", "file_progress", "progress"])
+                [])
             self._delive_torrent_parse_success(info["hash"], file_data, info)
         except RuntimeError as ex:
             logging.warning(
