ed6d40ae7e9bb1a04d99f143103ea93e6b26fc10
古娜 <shiti08@126.com>
2017-05-25 00:34:03 +0800

    fix t

--- clusterautodownloadplugin/eventpacher.py
+++ clusterautodownloadplugin/eventpacher.py
@@ -29,9 +29,10 @@ class EventPacher(object):
         torrent_manager.alerts.session.apply_settings(
             {'alert_mask': alert_mask})
         self.torrent_manager = torrent_manager
-        """
+
         torrent_manager.alerts.register_handler(
             'torrent_finished_alert', self.on_alert_torrent_finished)
+        """
         torrent_manager.alerts.register_handler(
             'file_completed_alert', self.on_alert_file_completed)
         """
